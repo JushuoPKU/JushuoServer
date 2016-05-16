ebb16d30f796490c9d818be8516a23d252a97d7f
mebiuw <wusixing@pku.edu.cn>
2016-05-16 23:26:00 +0800

    Test

---
+++ src/oxford/Configuration.java
@@ -0,0 +1,11 @@
+package azure.oxford;
+
+/**
+ * Created by MebiuW on 16/5/13.
+ */
+public class Configuration{
+/**
+ * TODO 请将Oxford用到的一些参数,用public static final String XXX的形式放在这里
+ */
+public static final String app_key="d4daf96d2b6e416fa91b3eebb144b2bd";
+}
